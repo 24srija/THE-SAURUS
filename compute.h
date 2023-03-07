3ae7f0cab87e0fa2df8ae7d96ab00a2357c84a18
24srija <127174755+24srija@users.noreply.github.com>
2023-03-07 10:27:05 +0530

    Add files via upload

---
+++ compute.h
@@ -0,0 +1,7 @@
+void startEngine(int highScore, struct user firstUser);
+int computePrize(int score, int usedPrize);
+void endGame(int score, int highScore, int diY, int diX, struct user firstUser);
+void showDinasour(int diY, int diX);
+void startMenu();
+int computeTime(int delayTime);
+int checkGame(int y, int x, int diY, int diX);
